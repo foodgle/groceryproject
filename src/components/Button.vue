11963227860efc95597b09935d38aed77e21f926
foodgle <foodgle@users.noreply.github.com>
2020-03-09 00:14:48 +0800

    Comments

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button v-on:click = increment()>+</button>
+            <button v-on:click = decrement()>-</button>
+            <p v-html = "counter"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data(){
+    return{
+        counter: 0
+        }
+  },
+  methods : {
+      increment:function() {
+          this.counter = this.counter + 1;
+      },
+
+      decrement:function() {
+          this.counter = this.counter - 1;
+      }
+  }
+}
+</script>
